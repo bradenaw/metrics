06f549d4c5509974eb3bfbe8ff713c8a3d65f935
Braden Walker <bradenawalker@gmail.com>
2023-06-06 02:35:04 -0400

    make NewBucketedCounter take *CounterDef1[string] because BindPrefix* made that easy

--- bucketed_counters.go
+++ bucketed_counters.go
@@ -12,38 +12,47 @@ type BucketedCounter struct {
 }
 
 func NewBucketedCounter(
-	c *Counter,
+	m *Metrics,
+	d *CounterDef1[string],
 	boundaries []float64,
 ) *BucketedCounter {
-	var counters []*Counter
-
 	if len(boundaries) == 0 {
-		counters = []*Counter{c}
-	} else {
-		// https://docs.datadoghq.com/getting_started/tagging/
-		//
-		// Tags must start with a letter and after that may contain the characters listed below:
-		//
-		// - Alphanumerics
-		// - Underscores
-		// - Minuses
-		// - Colons
-		// - Periods
-		// - Slashes
-		//
-		// [-./] all have meanings in numbers, colons already used for tag key:values, so that leaves alphanum and _
-
-		addCounter := func(tag string) *Counter {
-			return c.m.counter(c.name, append(c.tags[0:len(c.tags):len(c.tags)], tag))
+		return &BucketedCounter{
+			boundaries: nil,
+			counters:   []*Counter{d.Bind(m, "")},
 		}
-
-		counters = make([]*Counter, len(boundaries)+1)
-		counters[0] = addCounter(fmt.Sprintf("bucket:lt_%f", boundaries[0]))
-		for i := 1; i < len(boundaries); i++ {
-			counters[i] = addCounter(fmt.Sprintf("bucket:in_%f_%f", boundaries[i-1], boundaries[i]))
-		}
-		counters[len(boundaries)] = addCounter(fmt.Sprintf("bucket:gt_%f", boundaries[len(boundaries)-1]))
 	}
+
+	// https://docs.datadoghq.com/getting_started/tagging/
+	//
+	// > Tags must start with a letter and after that may contain the characters listed below:
+	// >
+	// > - Alphanumerics
+	// > - Underscores
+	// > - Minuses
+	// > - Colons
+	// > - Periods
+	// > - Slashes
+	//
+	// [-./] all have meanings in numbers, colons already used for tag key:values, so that leaves
+	// alphanum and _
+
+	counters := make([]*Counter, len(boundaries)+1)
+	counters[0] = d.Bind(m, fmt.Sprintf(
+		"bucket:lt_%f",
+		boundaries[0],
+	))
+	for i := 1; i < len(boundaries); i++ {
+		counters[i] = d.Bind(m, fmt.Sprintf(
+			"bucket:in_%f_%f",
+			boundaries[i-1],
+			boundaries[i],
+		))
+	}
+	counters[len(boundaries)] = d.Bind(m, fmt.Sprintf(
+		"bucket:gt_%f",
+		boundaries[len(boundaries)-1],
+	))
 
 	return &BucketedCounter{
 		boundaries: boundaries,
