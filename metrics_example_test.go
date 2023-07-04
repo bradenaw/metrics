ad71c681e082b892bda61ffb58dc75980c2cc314
Braden Walker <bradenawalker@gmail.com>
2023-07-03 23:17:48 -0400

    move examples for buckets to testing examples, fix them

--- metrics_example_test.go
+++ metrics_example_test.go
@@ -1,6 +1,7 @@
 package metrics_test
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/bradenaw/metrics"
@@ -62,4 +63,28 @@ func ExampleBucketedGaugeGroup() {
 
 		return s
 	}
+}
+
+func ExampleExponentialBuckets() {
+	buckets := metrics.ExponentialBuckets(100, 10, 3)
+	fmt.Println("ExponentialBuckets(100, 10, 3) ->", buckets)
+
+	buckets = metrics.ExponentialBuckets(100, 2, 5)
+	fmt.Println("ExponentialBuckets(100, 2, 5) ->", buckets)
+
+	// Output:
+	// ExponentialBuckets(100, 10, 3) -> [100 1000 10000]
+	// ExponentialBuckets(100, 2, 5) -> [100 200 400 800 1600]
+}
+
+func ExampleLinearBuckets() {
+	buckets := metrics.LinearBuckets(100, 50, 3)
+	fmt.Println("LinearBuckets(100, 50, 3) ->", buckets)
+
+	buckets = metrics.LinearBuckets(100, 75, 4)
+	fmt.Println("LinearBuckets(100, 75, 4) ->", buckets)
+
+	// Output:
+	// LinearBuckets(100, 50, 3) -> [100 150 200]
+	// LinearBuckets(100, 75, 4) -> [100 175 250 325]
 }
